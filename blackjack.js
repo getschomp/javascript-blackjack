1b626c5066fca3415a4a4a45a824ebc2ff37733b
Allison <allisonmariebrowne@gmail.com>
2015-02-22 20:05:22 -0500

    it makes a deck

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,28 @@
 // create a deck of 52 cards
 // use a prototype not a class to
-// create new cards and decks
+// create new cards and decks?
+function makeDeck() {
+  var numOfCards = 52;
+  var values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  var suits = ['spade', 'club', 'heart', 'dimond'];
+  var cards = [];
+  var card = { suit: "", value: "" }
+  for (i = 0; i < values.length; i++) {
+    for (j = 0; j < suits.length; j++) {
+      card = { value: values[i], suit: suits[j] };
+      cards.push(card);
+    }
+  }
+  var deck = {
+    cards: cards,
+    numOfCards: numOfCards
+  }
+  return deck;
+}
+
+deck = makeDeck();
+console.log(deck);
+// //function dealCards(number){
+// //}
+
+
